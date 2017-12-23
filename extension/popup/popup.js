e07f149d137c23ed9eb0e45c051e029b4df44720
Jun Park <jun.parkc@gmail.com>
2017-12-23 20:52:45 +0900

    Implement UI internationalization

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -194,7 +194,29 @@ function openRegistration() {
     chrome.tabs.create({ url: '../pages/registration/registration.html' })
 }
 
+function i18n() {
+    var i18nElems = document.getElementsByClassName('i18n');
+    var numElems = i18nElems.length;
+    while (numElems) {
+        var elem = i18nElems[numElems - 1];
+        var match = elem.className.match(/i18n_([\S]*)/)[1];
+        if (match) {
+            var type = match.substring(match.lastIndexOf('_') + 1);
+            if (type === 'button' || type === 'link') {
+                elem.value = chrome.i18n.getMessage(match);
+            } else if (type === 'ph') {
+                elem.placeholder = chrome.i18n.getMessage(match);
+            } else {
+                elem.innerHTML = chrome.i18n.getMessage(match);
+            }
+        }
+        numElems -= 1;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+    i18n();
+
     var registerBtn = document.getElementById('register-btn');
     var signupBtn = document.getElementById('signup-btn');
     var loginBtn = document.getElementById('login-btn');
